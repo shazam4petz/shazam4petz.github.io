e213a008bbc7202f379173c3af3920bb16de7b3c
Miffy Chen <mchen15@bu.edu>
2019-02-10 10:29:30 -0500

    Comment out flask on line 15

--- python/Anna_s_py.py
+++ python/Anna_s_py.py
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import escape
+# comment out for GitHub
+#from flask import escape
 
 def run_quickstart(request):
     #
